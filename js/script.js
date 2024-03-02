e5c9a12dfd4bebbb940c5ba4e735b04508c2ac20
Marshall Slaybod <69924240+marshall-slaybod@users.noreply.github.com>
2024-03-01 21:40:54 -0500

    Upload script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
+function toggleNav(element) {
+  var x = document.getElementById(element);
+  
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
+function toggleContent(element, control, nav) {
+  var x = document.getElementById(element);
+  var y = document.getElementById(control);
+  
+  if (x.style.display === "block") {
+	  if (!nav){
+		  y.classList.toggle("active");
+		  x.style.display = "none";
+	  } 
+  }
+  else {
+	y.classList.toggle("active");
+    x.style.display = "block";
+  }
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {
+	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	var scrolled = (winScroll / height) * 100;
+	document.getElementById("myBar").style.width = scrolled + "%";
+	document.getElementById("hamburger").style.display = "none";
+};
